50e79f1f3ec57fbdf8d93a1fc3a4beec5568c89d
tclaudel <contact@tclaudel.fr>
2023-09-06 14:08:50 +0200

    mongo_test.go

--- mongo_test.go
+++ mongo_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func TestMongoRepo_CreateUser(t *testing.T) {
+	ctx := context.Background()
 
+	repo, err := NewMongoRepo(ctx, "mongodb://localhost:27017")
+	if err != nil {
+		t.Fatalf("error creating mongo repo: %s", err)
+	}
+
+	user := &User{
+		ID:       primitive.NewObjectID(),
+		Name:     "John",
+		Email:    "john@example.com",
+		Password: "password",
+	}
+
+	err = repo.CreateUser(ctx, user)
+	if err != nil {
+		t.Fatalf("error creating user: %s", err)
+	}
 }
